03e47a60a6ff207d6f0cc6e1509c3199d39e619a
Илющенко Павел <pavel.ks1996@gmail.com>
2017-08-16 15:19:18 +0300

    initial commit

---
+++ program.py
@@ -0,0 +1,9 @@
+import logging
+import config
+
+def main():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+if __name__ == "__main__":
+    main()
